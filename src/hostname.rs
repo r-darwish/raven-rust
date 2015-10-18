2e75e1889474567d873fdabec5ce8b35f3e02062
Roey Darwish Dror <roeyd@infinidat.com>
2015-10-18 18:14:16 +0300

    Remove uneeded strlen

--- src/hostname.rs
+++ src/hostname.rs
@@ -5,16 +5,6 @@ extern {
     fn gethostname(name: *const u8, name_len: size_t) -> c_int;
 }
 
-fn strlen(s: &[u8]) -> usize {
-    for (i, c) in s.iter().enumerate() {
-        if *c == 0 {
-            return i;
-        }
-    }
-
-    s.len()
-}
-
 pub fn get_hostname() -> Option<String> {
     let buffer: [u8; 1024] = [0; 1024];
     unsafe {
@@ -23,7 +13,7 @@ pub fn get_hostname() -> Option<String> {
         }
     };
 
-    match String::from_utf8(buffer[..strlen(&buffer)].to_vec()) {
+    match String::from_utf8(buffer.to_vec()) {
         Ok(s) => Some(s),
         _ => None
     }
