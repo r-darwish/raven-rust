84ea64e9ec8072f6588f535c84d91f1ddfc35922
Roey Darwish Dror <roeyd@infinidat.com>
2015-10-21 16:12:24 +0300

    Implement Error for HostnameError

--- src/hostname.rs
+++ src/hostname.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+use std::error::Error;
 use std::string::FromUtf8Error;
 use libc::{c_int, size_t};
 
@@ -6,10 +8,38 @@ extern {
     fn gethostname(name: *const u8, name_len: size_t) -> c_int;
 }
 
+#[derive(Debug)]
 pub enum HostnameError {
     SystemError,
     FromUtf8Error(FromUtf8Error),
 }
+
+impl Error for HostnameError {
+    fn description(&self) -> &str {
+        match *self {
+            HostnameError::SystemError => "gethostname error",
+            HostnameError::FromUtf8Error(_) => "hostname cannot be decoded",
+        }
+    }
+
+    fn cause(&self) -> Option<&Error> {
+        match *self {
+            HostnameError::SystemError => None,
+            HostnameError::FromUtf8Error(ref e) => Some(e),
+        }
+    }
+}
+
+impl fmt::Display for HostnameError {
+    fn fmt(&self, fmt: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+        match *self {
+            HostnameError::SystemError => fmt.write_str("gethostname error"),
+            HostnameError::FromUtf8Error(ref e) => write!(fmt, "hostname cannot be encoded: {}", e),
+        }
+    }
+}
+
+
 
 impl From<FromUtf8Error> for HostnameError { fn from(e: FromUtf8Error) -> HostnameError { HostnameError::FromUtf8Error(e)} }
 
