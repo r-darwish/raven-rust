6fe60746691dec97daa86411996773faba9f4cb5
Roey Darwish Dror <roeyd@infinidat.com>
2018-04-22 15:31:33 +0300

    Abandon ship

---
+++ README.md
@@ -0,0 +1,1 @@
+This project is obsolete. Use the [official crate](https://github.com/getsentry/sentry-rust).
--- examples/message.rs
+++
@@ -1,9 +0,0 @@
-extern crate raven;
-
-use std::env;
-
-fn main() {
-    let client = raven::Client::from_string(&*env::var("SENTRY_DSN").ok().expect("missing SENTRY_DSN environment variable")).unwrap();
-    client.capture_message("Something aweful just happened", &[("version", "stable")]).unwrap();
-    println!("Success. Check Sentry for your error");
-}
